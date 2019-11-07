cdb29ed53014cfcbe64b4a99e8770eda284ab53c
Yasuhiro Noake <ynoake@gmail.com>
2019-11-07 10:32:17 +0000

    add css file

--- src/content.js
+++ src/content.js
@@ -2,12 +2,10 @@
   var elm = document.createElement("div");
   elm.id = "stalker";
   elm.innerText = location.host;
-  elm.style.color = 'black';
-  elm.style.position = 'fixed';
-  elm.style.zIndex = '2147483647';
   document.body.appendChild(elm);
   document.body.addEventListener("mousemove", function(event){
+    var hei = elm.clientHeight;
     elm.style.left = event.clientX + 'px';
-    elm.style.top = event.clientY - 42 + 'px';
+    elm.style.top = (event.clientY - (21 + hei)) + 'px';
   }, false);
 })();
